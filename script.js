eb88415dc50276c85d482aa53119a9c0bb22cdac
Lan Anh <anh.do1@onemount.com>
2021-05-07 14:15:46 +0700

    Lan Anh BTVN DOM

---
+++ script.js
@@ -0,0 +1,19 @@
+console.log("RUN SCRIPT");
+
+// Đổi style của tiêu đề Our companies thành chữ in đậm và kích thước 32px
+// Gợi ý: Sử dụng .style.fontWeight và .style.fontSize
+var x = document.getElementsByTagName("h2")[0]
+x.style.fontSize="32px"
+x.style.fontWeight="bold"
+// Đổi màu nền của phần footer thành màu #0984e3
+// Gợi ý: Sử dụng .style.backgroundColor
+var y = document.getElementById("OMG-footer")
+y.style.backgroundColor="#0984e3"
+// Đổi đoạn text ở footer từ "© 2020 One Mount Group, JSC." thành "© 2021 One Mount Group, JSC."
+// Gợi ý: Sử dụng .innerText
+y.innerText="© 2021 One Mount Group, JSC."
+// Đổi ảnh đầu tiên thành ảnh onhousing.png
+// Gợi ý: Sử dụng .src
+document.images[0].src="img/onhousing.png"
+
+
